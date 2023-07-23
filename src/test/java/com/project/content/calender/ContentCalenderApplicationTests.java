a9d0f76556137a9423d7cf0785507b15e73b0847
teledi-makhafola <lkgahliso03@gmail.com>
2023-07-23 18:52:54 +0200

    first initial commit

---
+++ src/test/java/com/project/content/calender/ContentCalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.content.calender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
